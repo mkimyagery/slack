bca63ed39625bf61b188734f919eea815e2e19bb
mkimyagery <mkimyagery@gmail.com>
2020-05-12 20:32:59 +0200

    b

---
+++ src/day_1/Exercisee.java
@@ -0,0 +1,12 @@
+package day_1;
+
+public class Exercisee {
+
+    public static void main(String[] args) {
+        System.out.println(" i am pushing my code to gitup ");
+        // git add .
+        // git commit -m "new code "
+        // git push
+
+    }
+}
--- src/day_1/login.java
+++ src/day_1/login.java
@@ -4,7 +4,10 @@ public class login {
     public static void main(String[] args) {
         System.out.println("just practice here");
         
-        
+        // git fetch
+        // git merge
+        // git pull
+
         
          System.out.println(" I am edditing from github page ");
         
