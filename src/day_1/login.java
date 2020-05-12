11d04b748eb19ebc588968ded8897ef0283ac942
mkimyagery <59318514+mkimyagery@users.noreply.github.com>
2020-05-12 18:23:43 +0200

    Update login.java

--- src/day_1/login.java
+++ src/day_1/login.java
@@ -3,5 +3,10 @@ package day_1;
 public class login {
     public static void main(String[] args) {
         System.out.println("just practice here");
+        
+        
+        
+         System.out.println(" I am edditing from github page ");
+        
     }
 }
