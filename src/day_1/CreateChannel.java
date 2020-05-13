04210cee4b4fd92e18062d6a8d339a4c8b5159d7
mkimyagery <mkimyagery@gmail.com>
2020-05-13 16:55:47 +0200

    changed

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,5 +4,9 @@ public class CreateChannel {
 
     public static void main(String[] args) {
         System.out.println("user is able to create a channel ");
+
+
+        System.out.println("i am adding new line in intelligie ");
+
     }
 }
